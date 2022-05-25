f6646d2841c75404e8b5b966fb8855544d658ac1
NeurIPS-2022 <105804347+NeurIPS-2022@users.noreply.github.com>
2022-05-25 20:56:31 +0900

    Add files via upload

---
+++ DVFN_PO/standard/dvfn_main.py
@@ -0,0 +1,42 @@
+from module.dvfn_engine import DVFN
+import numpy as np
+from problems.production_optimization import make_problem_definition
+
+def solve_PO(n_stages=7,
+             n_nodes=64,
+             n_hlayers=1,
+             n_epochs=30,
+             ICNN_optimizer="Adam",
+             lr=0.0015,
+             min_iter=50,
+             max_iter=50):
+
+    problem_definition = make_problem_definition(n_stages=n_stages)
+    solver = DVFN(problem_definition,
+                  n_nodes,
+                  n_hlayers,
+                  n_epochs,
+                  ICNN_optimizer,
+                  lr,
+                  min_iter,
+                  max_iter)
+
+    sol, obj = solver.solve()
+
+    return sol, obj
+
+# User defined setting
+n_stages = 7
+n_nodes = 64
+n_hlayers = 1
+n_epochs = 30
+ICNN_optimizer = "Adam"
+lr = 0.0015
+min_iter = 5
+max_iter = 5
+
+sol, obj = solve_PO(n_stages, n_nodes, n_hlayers, n_epochs, ICNN_optimizer, lr, min_iter, max_iter)
+print("Production: ", np.reshape(sol, sol.shape[0])[0:3])
+print("Outsource: ", np.reshape(sol, sol.shape[0])[3:6])
+print("Storage: ", np.reshape(sol, sol.shape[0])[6:9])
+print("Objective value: ", obj)
