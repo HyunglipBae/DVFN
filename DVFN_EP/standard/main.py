443a6234fe07a157f82e6908d9d78b2fc10d22b0
AISTATS-2023 <105804347+AISTATS-2023@users.noreply.github.com>
2022-10-23 23:34:14 +0900

    Update main.py

--- DVFN_EP/standard/main.py
+++ DVFN_EP/standard/main.py
@@ -2,14 +2,14 @@ from module.dvfn_engine import DVFN
 import numpy as np
 from problems.energy_planning import make_problem_definition
 
-def solve_EP(n_stages=7,
-             n_nodes=128,
+def solve_EP(n_stages=15,
+             n_nodes=64,
              n_hlayers=1,
-             n_epochs=20,
+             n_epochs=5,
              ICNN_optimizer="Adam",
              lr=0.001,
-             min_iter=200,
-             max_iter=200):
+             min_iter=150,
+             max_iter=150):
 
     problem_definition = make_problem_definition(n_stages=n_stages)
     solver = DVFN(problem_definition,
@@ -26,14 +26,14 @@ def solve_EP(n_stages=7,
     return sol, obj
 
 # User defined setting
-n_stages = 7
-n_nodes = 128
+n_stages = 11
+n_nodes = 64
 n_hlayers = 1
-n_epochs = 20
+n_epochs = 5
 ICNN_optimizer = "Adam"
 lr = 0.001
-min_iter = 200
-max_iter = 200
+min_iter = 150
+max_iter = 150
 
 sol, obj = solve_EP(n_stages, n_nodes, n_hlayers, n_epochs, ICNN_optimizer, lr, min_iter, max_iter)
 print("Hydro: ", np.reshape(sol, sol.shape[0])[2])
